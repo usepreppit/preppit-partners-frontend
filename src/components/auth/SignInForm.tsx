4687f9cd2aaabfb3c0a767655cdf4f3d66609c97
daybaryour <debaryour@gmail.com>
2025-11-30 23:55:14 +0100

    FIX: Signin Button bUg

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -119,6 +119,7 @@ export default function SignInForm() {
                 </div>
                 <div>
                   <Button 
+                    type="submit"
                     className="w-full" 
                     size="sm"
                     disabled={loginMutation.isPending}
